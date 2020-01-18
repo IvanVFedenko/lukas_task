a13761fe4ae7d5ce4b374120f7756379910d0a6f
IvanVFedenko <ivan.v.fedenko@gmail.com>
2020-01-18 19:49:29 +0200

    Updates

---
+++ precache-manifest.2258f41f2faae6798246314d877f3f2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "944ae82173a6e0c9ccffc91a809e81f6",
+    "url": "/lukas_task/index.html"
+  },
+  {
+    "revision": "a65bf2a5bc47b4a3084d",
+    "url": "/lukas_task/static/css/main.eacf3af0.chunk.css"
+  },
+  {
+    "revision": "75540e2e1eae14da605e",
+    "url": "/lukas_task/static/js/2.1b0b5103.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/lukas_task/static/js/2.1b0b5103.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a65bf2a5bc47b4a3084d",
+    "url": "/lukas_task/static/js/main.d532f67a.chunk.js"
+  },
+  {
+    "revision": "21b2a4433b0e88712b55",
+    "url": "/lukas_task/static/js/runtime-main.d2998a15.js"
+  }
+]);
